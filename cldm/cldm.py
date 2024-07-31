ad6d51421b581d802cc309489fda0c1a32602276
Abdullah <88815725+Theonewhomadethings@users.noreply.github.com>
2024-07-31 18:05:37 +0100

    bug fix that will probs break it more

--- cldm/cldm.py
+++ cldm/cldm.py
@@ -359,7 +359,9 @@ class ControlLDM(LatentDiffusion):
         n_row = min(z.shape[0], n_row)
         log["reconstruction"] = self.decode_first_stage(z)
         log["control"] = c_cat * 2.0 - 1.0
-        log["conditioning"] = log_txt_as_img((512, 512), batch[self.cond_stage_key], size=16)
+        font_path = '/mnt/fast/nobackup/users/as05728/ControlNet/font/DejaVuSans.ttf'  # Adjust the path if necessary
+        log["conditioning"] = log_txt_as_img((512, 512), batch[self.cond_stage_key], size=16, font_path=font_path)
+        
 
         if plot_diffusion_rows:
             # get diffusion row
